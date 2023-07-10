9464142515dab831e7622de735a6b4ae7e1beee5
joshuanelsondev <joshuanelsondev@gmail.com>
2023-07-09 20:49:24 -0400

    Begin App.jsx set up and create a Theme.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+// App.jsx
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { applyTheme, setTheme } from "../../Theme";
 
-import './App.css'
+import "./App.css";
 
 function App() {
+  const [errorPage, setErrorPage] = useState(false);
 
+  useEffect(() => {
+    applyTheme();
+  }, []);
+
+  const handleThemeChange = (theme) => {
+    setTheme(theme);
+    applyTheme();
+  }
   return (
     <>
       <div>Welcome to my Portfolio</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
