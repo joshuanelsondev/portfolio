a870545a3b4aba71cf6cc0198f187088ed05f34c
joshuanelsondev <joshuanelsondev@gmail.com>
2023-07-10 00:13:05 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
+    },
+    colors: {
+      dark: "#021d28",
+      primary: "#1F7A8C",
+      secondary: "#D1E3DD",
+      blue: "#BFDBF7",
+      gray: "#E1E5F2",
+      white: "#FFFFFF",
+      red: "#E73B3B",
+    },
   },
   plugins: [],
+  darkMode: "class",
 };
 
