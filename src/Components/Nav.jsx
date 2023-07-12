21413c0181912ac959d889203b575bb35cc81cab
joshuanelsondev <joshuanelsondev@gmail.com>
2023-07-11 22:38:01 -0400

    Edit the responsive nav menu

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -111,6 +111,107 @@ export default function Nav({ handleThemeChange }) {
           </div>
         </div>
       </div>
+      <div className="h-10 w-10 flex justify-center fixed top-6 left-4">
+        <div className="z-10 lg:hidden">
+          <AiOutlineMenu
+            onClick={toggleNav}
+            className="text-primary dark:text-secondary transition-colors duration-300 ease-in-out transform hover:scale-110 cursor-pointer"
+            size={20}
+          />
+        </div>
+        {showNav && (
+          <div className="fixed top-9 ml-20 w-fit pr-4 z-40 h-fit bg-primary transition-transform duration-1000 ease-in-out transform translate-y-4 lg:hidden">
+            <div
+              className={`flex items-center gap-1 group cursor-pointer ${
+                currentPage("/") ? "text-dark" : "text-secondary"
+              }`}
+              onClick={() => navAndToggle("/")}
+            >
+              <MdHome
+                className="group-hover:text-dark rounded-full p-2"
+                size={30}
+              />
+              <p className="text-sm font-semibold group-hover:text-dark hover:scale-110">
+                Home
+              </p>
+            </div>
+            <div
+              className={`flex items-center gap-1 group cursor-pointer ${
+                currentPage("/about") ? "text-dark" : "text-secondary"
+              }`}
+              onClick={() => navAndToggle("/about")}
+            >
+              <MdAccountCircle
+                className="group-hover:text-dark rounded-full p-2"
+                size={30}
+              />
+              <p className="text-sm font-semibold group-hover:text-dark hover:scale-110">
+                About
+              </p>
+            </div>
+            <div
+              className={`flex items-center gap-1 group cursor-pointer ${
+                currentPage("/projects") ? "text-dark" : "text-secondary"
+              }`}
+              onClick={() => navAndToggle("/projects")}
+            >
+              <AiOutlineAppstore
+                className="group-hover:text-dark p-[8px] rounded-full"
+                size={30}
+              />
+              <p className="text-sm font-semibold group-hover:text-dark hover:scale-110">
+                Portfolio
+              </p>
+            </div>
+            <div
+              className={`flex items-center gap-1 group cursor-pointer ${
+                currentPage("/contact") ? "text-dark" : "text-secondary"
+              }`}
+              onClick={() => navAndToggle("/contact")}
+            >
+              <MdEmail
+                className="group-hover:text-dark rounded-full p-2"
+                size={30}
+              />
+              <p className="text-sm font-semibold group-hover:text-dark hover:scale-110">
+                Contact
+              </p>
+            </div>
+            <div className="relative">
+              <div
+                className={`invisible dark:visible flex items-center gap-1 group hover:text-dark cursor-pointer absolute`}
+              >
+                <MdOutlineDarkMode
+                  onClick={() => handleThemeChange('dark')}
+                  className="rounded-full p-2 group-hover:scale-110 text-secondary group-hover:text-dark"
+                  size={30}
+                />
+                <p
+                  className="text-sm text-secondary font-semibold hover:scale-110 group-hover:scale-110 group-hover:text-dark"
+                  onClick={() => handleThemeChange('dark')}
+                >
+                  Dark
+                </p>
+              </div>
+              <div
+                className={`dark:invisible flex items-center gap-1 group cursor-pointer hover:text-dark text-secondary`}
+              >
+                <MdOutlineLightMode
+                  onClick={() => handleThemeChange('light')}
+                  className="rounded-full p-2 group-hover:scale-110"
+                  size={30}
+                />
+                <p
+                  className="text-sm font-semibold hover:scale-110 group-hover: group-hover:scale-110"
+                  onClick={() => handleThemeChange('light')}
+                >
+                  Light
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
