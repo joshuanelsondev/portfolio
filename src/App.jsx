47ed8ac3070ea1a1235dbe0857f821717530ad05
joshuanelsondev <joshuanelsondev@gmail.com>
2023-07-12 23:57:28 -0400

    Add name log - work on its visibiity

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App.jsx
 
 // DEPENDENCIES
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { applyTheme, setTheme } from "./Theme";
 
@@ -21,19 +21,25 @@ import Nav from "./Components/Nav";
 
 function App() {
   const [errorPage, setErrorPage] = useState(false);
+  const path = location.pathname;
 
   useEffect(() => {
     applyTheme();
+    console.log(path)
   }, []);
 
   const handleThemeChange = (theme) => {
     setTheme(theme);
     applyTheme();
   }
+
   return (
     <main className="h-full w-full bg-gradient-to-b from-[#dfe9f3] via-white to-[#dfe9f3] dark:bg-gradient-to-b dark:from-dark dark:via-primary dark:to-dark">
       <Router>
         {!errorPage && <Nav handleThemeChange={handleThemeChange} />}
+        <div className={`text-primary dark:text-blue text-2xl text-shadow text-right mr-4 ${path === '/' && 'invisible'} visible`}>
+          Joshua Nelson
+        </div>
         <Routes>
           <Route path="/" element={<Home setErrorPage={setErrorPage} />} />
           <Route path="/about" element={<About />} />
