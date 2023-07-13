f6375183d31d72c87159e3ed576603c62a8f0a6b
joshuanelsondev <joshuanelsondev@gmail.com>
2023-07-13 10:16:50 -0400

    Update name logo

--- src/App.jsx
+++ src/App.jsx
@@ -21,23 +21,21 @@ import Nav from "./Components/Nav";
 
 function App() {
   const [errorPage, setErrorPage] = useState(false);
-  const path = location.pathname;
 
   useEffect(() => {
     applyTheme();
-    console.log(path)
-  }, []);
+  }); 
 
   const handleThemeChange = (theme) => {
     setTheme(theme);
     applyTheme();
   }
-
+ 
   return (
     <main className="h-full w-full bg-gradient-to-b from-[#dfe9f3] via-white to-[#dfe9f3] dark:bg-gradient-to-b dark:from-dark dark:via-primary dark:to-dark">
       <Router>
         {!errorPage && <Nav handleThemeChange={handleThemeChange} />}
-        <div className={`text-primary dark:text-blue text-2xl text-shadow text-right mr-4 ${path === '/' && 'invisible'} visible`}>
+        <div className={`text-primary dark:text-blue text-2xl text-shadow text-right mr-4`}>
           Joshua Nelson
         </div>
         <Routes>
