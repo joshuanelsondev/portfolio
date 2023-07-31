d33483fb564db2eb5f0964b871995d86e73678b5
joshuanelsondev <joshuanelsondev@gmail.com>
2023-07-31 00:11:38 -0400

    Edit nav bar for smaller screens; add a sliding nav. Need to fix the bug for the portfolio button. Need to edit code, put the data for each button into an array and map through them for cleaner code.

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -10,6 +10,7 @@ import {
   MdEmail,
   MdOutlineDarkMode,
   MdOutlineLightMode,
+  MdWest,
 } from "react-icons/md";
 import {
   AiOutlineAppstore,
@@ -159,19 +160,34 @@ export default function Nav({ handleThemeChange }) {
         </div>
       </div>
       {/* Hamburger Menu */}
-      <div className="border border-primary flex items-center gap-8 h-20 w-full">
-        <div className="z-10 lg:hidden">
-          <AiOutlineMenu
-            onClick={toggleNav}
-            className="text-primary dark:text-primary transition-colors duration-300 ease-in-out transform hover:scale-110 cursor-pointer"
-            size={24}
-          />
-        </div>
+
+      <div>
+        <AiOutlineMenu
+          onClick={toggleNav}
+          className={`fixed text-primary hover:scale-110 cursor-pointer left-5 top-4 ${
+            showNav
+              ? " duration-500 ease-out -translate-x-10"
+              : "ease-in duration-500"
+          }`}
+          size={24}
+        />
+      </div>
+      <div
+        className={`fixed -left-20 bg-secondary bg-opacity-90 flex flex-col items-center pt-4 h-full w-20 z-50 lg:hidden ${
+          showNav
+            ? "duration-500 ease-in translate-x-20"
+            : "duration-500 ease-out"
+        }`}
+      >
+        <MdWest
+          className="text-gray hover:scale-110 cursor-pointer left-5 top-4"
+          onClick={toggleNav}
+          size={28}
+        />
+
         {/* Smaller screen nav bar */}
-        {showNav && (
-          <div
-            className='flex'
-          >
+        {
+          <div className={`flex flex-col h-full w-20 mt-24 gap-8 top-10`}>
             {/* Home nav button */}
             <div className="flex flex-col items-center">
               <LinkScroll
@@ -184,15 +200,11 @@ export default function Nav({ handleThemeChange }) {
                 duration={500}
               >
                 <MdHome
-                  className="text-primary dark:text-gray rounded-full hover:outline hover:dark:outline-primary p-2 cursor-pointer"
+                  className="text-gray rounded-full hover:scale-105 hover:outline outline-primary p-2 cursor-pointer"
                   size={40}
                 />
               </LinkScroll>
-              <p className="invisible text-primary dark:text-primary font-semibold peer-hover:visible">
-                Home
-              </p>
             </div>
-
             {/* About nav button */}
             <div className="flex flex-col items-center">
               <LinkScroll
@@ -205,33 +217,26 @@ export default function Nav({ handleThemeChange }) {
                 duration={500}
               >
                 <MdAccountCircle
-                  className="text-primary dark:text-gray rounded-full hover:outline hover:dark:outline-primary p-2 cursor-pointer"
+                  className="text-gray rounded-full hover:scale-105 hover:outline outline-primary p-2 cursor-pointer"
                   size={40}
                 />
               </LinkScroll>
-              <p className="invisible text-primary dark:text-primary font-semibold peer-hover:visible">
-                About
-              </p>
             </div>
             {/* Portfolio nav button */}
             <div className="flex flex-col items-center">
               <LinkScroll
                 className="peer"
                 activeClass="outline rounded-full text-primary"
-                to="projects"
                 spy={true}
                 smooth={true}
                 offset={0}
                 duration={500}
               >
                 <AiOutlineAppstore
-                  className="text-primary dark:text-gray rounded-full hover:outline hover:dark:outline-primary p-2 cursor-pointer"
+                  className="text-gray rounded-full hover:scale-105 hover:outline outline-primary p-2 cursor-pointer"
                   size={40}
                 />
               </LinkScroll>
-              <p className="invisible text-primary dark:text-primary font-semibold peer-hover:visible">
-                Portfolio
-              </p>
             </div>
             {/* Contact nav button */}
             <div className="flex flex-col items-center">
@@ -245,41 +250,35 @@ export default function Nav({ handleThemeChange }) {
                 duration={500}
               >
                 <MdEmail
-                  className="text-primary dark:text-gray rounded-full hover:outline hover:dark:outline-primary p-2 cursor-pointer"
+                  className="text-gray rounded-full hover:scale-105 hover:outline outline-primary p-2 cursor-pointer"
                   size={40}
                 />
               </LinkScroll>
-              <p className="invisible text-primary dark:text-primary font-semibold peer-hover:visible">
+              {/* <p className="invisible text-primary dark:text-primary font-semibold peer-hover:visible">
                 Contact
-              </p>
+              </p> */}
             </div>
-
-            <div className="relative">
+            {/* Theme buttons */}
+            <div className="relative flex">
               {/* Dark theme nav button */}
-              <div className="absolute invisible dark:visible flex flex-col items-center">
+              <div className="absolute invisible dark:visible flex flex-col items-center left-5">
                 <MdOutlineDarkMode
                   onClick={() => handleThemeChange("dark")}
                   className="peer text-gray hover:outline hover:dark:outline-primary rounded-full p-2 cursor-pointer"
                   size={40}
                 />
-                <p className="invisible text-primary font-semibold peer-hover:visible">
-                  Dark
-                </p>
               </div>
               {/* Light theme nav button */}
-              <div className="absolute dark:invisible flex flex-col items-center">
+              <div className="absolute dark:invisible flex flex-col items-center left-5">
                 <MdOutlineLightMode
                   onClick={() => handleThemeChange("light")}
-                  className="peer text-primary hover:outline  rounded-full p-2 cursor-pointer"
+                  className="peer text-gray hover:outline outline-primary rounded-full p-2 cursor-pointer"
                   size={40}
                 />
-                <p className="invisible text-primary font-semibold peer-hover:visible">
-                  Light
-                </p>
               </div>
             </div>
           </div>
-        )}
+        }
       </div>
     </div>
   );
