aa86341558c802548805955652314483c15ef666
joshuanelsondev <joshuanelsondev@gmail.com>
2023-08-02 00:28:37 -0400

    Refactor HomeComponent

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -2,6 +2,7 @@
 
 import PropTypes from "prop-types";
 import { useEffect } from "react";
+import { v4 as generateId } from "uuid";
 import {
   AiFillLinkedin,
   AiOutlineGithub,
@@ -9,6 +10,29 @@ import {
   AiFillFacebook,
 } from "react-icons/ai";
 
+
+const icons = [
+  {
+    id: generateId(),
+    logo: AiFillLinkedin,
+    link: "https://www.linkedin.com/in/joshuanelsondev",
+  },
+  {
+    id: generateId(),
+    logo: AiOutlineGithub,
+    link: "https://github.com/joshuanelsondev",
+  },
+  {
+    id: generateId(),
+    logo: AiFillFacebook,
+    link: "https://www.facebook.com/JoshuaNelsonSax",
+  },
+  {
+    id: generateId(),
+    logo: AiFillInstagram,
+    link: "https://www.instagram.com/joshuanelsonmusic/",
+  },
+];
 export default function HomeComponent({ setShowNav }) {
     useEffect(() => {
     setShowNav(true);
@@ -31,73 +55,24 @@ export default function HomeComponent({ setShowNav }) {
             <span className="text-primary dark:text-gray">Web</span> Developer
           </h2>
           <div className="flex mt-4 gap-8 justify-center">
-            <div className="flex flex-col items-center gap-2">
-              <a
-                className="peer"
-                href="https://www.linkedin.com/in/joshuanelsondev"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiFillLinkedin
-                  className="p-2 text-primary dark:text-gray cursor-pointer hover:border-b border-b-primary"
-                  size={40}
-                />
-              </a>
-              {/* <p className="text-primary dark:text-primary font-semibold invisible peer-hover:visible">
-                LinkedIn
-              </p> */}
-            </div>
-
-            <div className="flex flex-col items-center gap-2">
-              <a
-                className="peer"
-                href="https://github.com/joshuanelsondev"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiOutlineGithub
-                  className="p-2 text-primary hover:border-b border-b-primary dark:text-gray cursor-pointer"
-                  size={40}
-                />
-              </a>
-              {/* <p className="text-primary dark:text-primary font-semibold invisible peer-hover:visible">
-                GitHub
-              </p> */}
-            </div>
-
-            <div className="flex flex-col items-center gap-2">
-              <a
-                className="peer"
-                href="https://www.facebook.com/JoshuaNelsonSax"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiFillFacebook
-                  className="p-2 text-primary dark:text-gray cursor-pointer hover:border-b border-b-primary"
-                  size={40}
-                />
-              </a>
-              {/* <p className="text-primary dark:text-primary font-semibold invisible peer-hover:visible">
-                Facebook
-              </p> */}
-            </div>
-
-            <div className="flex flex-col items-center gap-2">
-              <a
-                className="peer"
-                href="https://www.instagram.com/joshuanelsonmusic/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <AiFillInstagram
-                  className="p-2 text-primary dark:text-gray cursor-pointer hover:border-b border-b-primary"
-                  size={40}
-                />
-              </a>
-              {/* <p className="text-primary dark:text-primary font-semibold invisible peer-hover:visible">
-                Instagram
-              </p> */}
-            </div>
+            {icons.map(icon => {
+              return (
+                <div key={icon.id} className="flex flex-col items-center gap-2">
+                  <a
+                    className="peer"
+                    href={`${icon.link}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <icon.logo
+                      className="p-2 text-primary dark:text-gray cursor-pointer hover:border-b border-b-primary"
+                      size={40}
+                    />
+                  </a>
+                </div>
+              );
+            })}
+            
           </div>
         </div>
       </div>
