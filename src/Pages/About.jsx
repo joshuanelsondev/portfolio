7c72ab2955fb11c67e6be99c0c45f28cce831e4d
joshuanelsondev <joshuanelsondev@gmail.com>
2023-08-04 08:27:04 -0400

    Edit flex properties of About page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,7 +3,7 @@ import AboutMe from "../Components/AboutMe";
 
 export default function About() {
     return (
-      <div id="about" className="px-10 lg:px-0 lg:mx-20 h-full w-auto pt-20">
+      <div id="about" className="flex justify-evenly px-10 lg:px-0 lg:mx-20 h-full w-auto lg:pt-20">
         <AboutMe />
       </div>
     );
