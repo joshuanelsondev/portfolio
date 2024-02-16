d1cce24c68904f67a947f0ce3d28dd3aea4bc474
joshuanelsondev <joshuanelsondev@gmail.com>
2024-02-15 19:01:32 -0500

    Edit HomeComponent

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -103,7 +103,7 @@ export default function Nav({ handleThemeChange }) {
       </div>
       {/* Smaller screen nav bar */}
       <div
-        className={`fixed  -left-20 top-0 bg-primary bg-opacity-90 flex flex-col items-center pt-4 h-full w-10 z-50 lg:hidden ${
+        className={`fixed bg-primary bg-opacity-90 -left-20 top-0 flex flex-col items-center h-full w-10 pt-4  z-50 lg:hidden ${
           showMiniNav
             ? "duration-500 ease-in translate-x-20"
             : "duration-500 ease-in-out"
@@ -143,7 +143,7 @@ export default function Nav({ handleThemeChange }) {
               {/* Dark theme nav button */}
               <div className="absolute invisible dark:visible">
                 <MdOutlineDarkMode
-                  onClick={() => handleThemeChange("dark")}
+                  onClick={() => handleThemeChange("light")}
                   className="peer text-gray hover:outline hover:dark:outline-gray rounded-full p-1 cursor-pointer"
                   size={25}
                 />
@@ -151,7 +151,7 @@ export default function Nav({ handleThemeChange }) {
               {/* Light theme nav button */}
               <div className="absolute dark:invisible">
                 <MdOutlineLightMode
-                  onClick={() => handleThemeChange("light")}
+                  onClick={() => handleThemeChange("dark")}
                   className="peer text-gray hover:outline outline-gray rounded-full p-1 cursor-pointer"
                   size={25}
                 />
