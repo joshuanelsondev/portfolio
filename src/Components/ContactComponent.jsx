f2ba0e19fde54c457f26da8eb6a1025e9f924bb0
joshuanelsondev <joshuanelsondev@gmail.com>
2024-02-21 19:29:41 -0500

    Edit contact form functionality

--- src/Components/ContactComponent.jsx
+++ src/Components/ContactComponent.jsx
@@ -1,48 +1,56 @@
 // ContactComponent.jsx
-import { useRef } from "react";
+import { useState, useRef } from "react";
 import emailjs from "@emailjs/browser";
 
+
 export default function ContactComponent () {
-    const form = useRef();
+  const [btnMessage, setBtnMessage] = useState('Send Email')  
 
-    const sendEmail = (e) => {
-      e.preventDefault();
+  const form = useRef();
 
-      emailjs
-        .sendForm(
-          "service_091zju5",
-          "template_kx2no28",
-          form.current,
-          "OBvKElsFehAWv_TZS"
-        )
-        .then(
-          (result) => {
-            console.log(result.text);
-          },
-          (error) => {
-            console.log(error.text);
-          }
-        );
-    };
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setBtnMessage('Sending...');
+
+    emailjs
+      .sendForm(
+        "service_091zju5",
+        "template_kx2no28",
+        form.current,
+        "OBvKElsFehAWv_TZS"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setInterval(() => {
+            setBtnMessage('Send Email');
+          }, 1000)
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
     return (
-      <div id='contact' className="h-full min-h-[600px] w-full flex flex-col text-white ">
+      <div id='contact' className="h-full min-h-[600px] w-full flex flex-col text-white">
         <h2 className="text-4xl lg:text-6xl text-primary h-20">Contact me</h2>
-        <form className="flex flex-col gap-8" ref={form} onSubmit={sendEmail}>
+        <form className="flex flex-col gap-8 rounded-md" ref={form} onSubmit={sendEmail}>
           <div className='grid gap-2'>
-            <label>Name</label>
-            <input className='rounded text-black px-2' type="text" name="user_name" />
+            <label htmlFor='from_name' className='text-black dark:text-white'>Name</label>
+            <input name='from_name' className='rounded text-black px-2 outline outline-gray' type="text" required />
           </div> 
           <div className='grid gap-2'>
-            <label>Email</label>
-            <input className='rounded text-black' type="email" name="user_email" />
+            <label className='text-black dark:text-white'>Email</label>
+            <input className='rounded text-black px-2 outline outline-gray' type="email" name="user_email" required />
           </div> 
           <div className='grid gap-2'>
-            <label>Message</label>
-            <textarea className='rounded h-40 text-black' name="message" />
+            <label htmlFor='message' className='text-black dark:text-white'>Message</label>
+            <textarea name='message' className='rounded h-40 text-black px-2 outline outline-gray' required />
           </div> 
-          <input className='bg-primary rounded p-2 text-darkGray font-semibold' type="submit" value="Send" />
+          <input className='bg-primary rounded p-2 text-darkGray font-semibold' type="submit" value={btnMessage} />
         </form>
       </div>
     );
 }
+
