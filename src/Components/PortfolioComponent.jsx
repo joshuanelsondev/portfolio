c95dcc0c63ccd52d332dc44a02aec454c7dd3773
joshuanelsondev <joshuanelsondev@gmail.com>
2024-03-04 18:45:06 -0500

    Edit portfolio layout

--- src/Components/PortfolioComponent.jsx
+++ src/Components/PortfolioComponent.jsx
@@ -14,7 +14,7 @@ export default function PortfolioComponent() {
     return (
       <div id='portfolio' className="h-auto min-h-[600px] w-auto flex flex-col items-center mb-12 md:mb-20 md:mt-24">
         <h1 className="text-4xl lg:text-6xl text-primary mb-4 md:mb-10">Portfolio</h1>
-        <div className="flex flex-col lg:grid lg:grid-cols-2 gap-8">
+        <div className="flex flex-wrap place-content-center px-40 gap-20">
           {projects.map((project) => (
             <div key={project.id} className="cursor-pointer">
               <Project project={project} />
