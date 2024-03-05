5047f12025385475af228e66b4de83d31541f6e9
joshuanelsondev <joshuanelsondev@gmail.com>
2024-03-05 15:19:11 -0500

    Update Project.jsx to include lazy loading, placeholder images, error message, and add aria-label

--- src/Components/PortfolioComponent.jsx
+++ src/Components/PortfolioComponent.jsx
@@ -13,7 +13,7 @@ export default function PortfolioComponent() {
 
     return (
       <div id='portfolio' className="h-auto min-h-[600px] w-auto flex flex-col items-center mb-12 md:mb-20 md:mt-24">
-        <h1 className="text-4xl lg:text-6xl text-primary mb-4 md:mb-10">Portfolio</h1>
+        <h1 className="text-4xl lg:text-6xl font-semibold text-darkGray dark:text-primary mb-4 md:mb-10">Portfolio</h1>
         <div className="flex flex-wrap place-content-center px-40 gap-20">
           {projects.map((project) => (
             <div key={project.id} className="cursor-pointer">
--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,24 +1,40 @@
 // Project.jsx
 
 import PropTypes from "prop-types";
+import { useState } from 'react';
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "react-router-dom";
 import { AiFillGithub, AiOutlineExport } from "react-icons/ai";
 
 export default function Project({ project }) {
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+  const handleError = (event) => {
+    event.target.src = `${project.placeholder_image}`
+    setShowErrorMessage(true);
+  }
+
+
   return (
     <div className="max-w-sm p-2 z-10 relative">
-        <img
-          src={`${project.image}`}
-          alt={`${project.title} - Error Loading`}
-          onError={this.src=''}
-          className="rounded-md text-primary h-60 w-[500px] box-border"
-        />
+      {showErrorMessage && (
+        <p aria-label={`Error loading ${project.title} project`} className='text-darkGray dark:text-primary font-semibold text-center mb-2'>Error Loading - {project.title}</p>
+      )}
+      <LazyLoadImage 
+        src={`${project.image}`} 
+        alt={`${project.title} - Error Loading`} 
+        onError={handleError}
+        placeholderSrc={`${project.placeholder_image}`} 
+        className="rounded-md text-primary h-60 w-[500px] box-border" 
+      />
     </div>
   );
 }
 
 Project.propTypes = {
-    project: PropTypes.object.isRequired
+  project: PropTypes.shape({
+    image: PropTypes.string,
+    placeholder_image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
 }
-
-
