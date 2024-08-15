c67a5e6b0208d812e59249cc15fb1a5a949a1619
joshuanelsondev <joshuanelsondev@gmail.com>
2024-08-15 00:06:43 -0400

    refactor: update styling for ContactComponent and PortfolioComponent

--- src/Components/ContactComponent.jsx
+++ src/Components/ContactComponent.jsx
@@ -1,64 +1,89 @@
 // ContactComponent.jsx
-import { useState, useRef } from "react";
-import emailjs from "@emailjs/browser";
+import { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
-const serviceId = import.meta.env.VITE_SERVICE_ID;
-const templateId = import.meta.env.VITE_TEMPLATE_ID;
-const apiKey = import.meta.env.VITE_API_KEY;
+const serviceId = import.meta.env.VITE_SERVICE_ID
+const templateId = import.meta.env.VITE_TEMPLATE_ID
+const apiKey = import.meta.env.VITE_API_KEY
 
-export default function ContactComponent () {
-  const [btnMessage, setBtnMessage] = useState('Send Email')  
+export default function ContactComponent() {
+  const [btnMessage, setBtnMessage] = useState('Send Email')
 
-  const form = useRef();
+  const form = useRef()
 
   const sendEmail = (e) => {
-    e.preventDefault();
-    setBtnMessage('Sending...');
+    e.preventDefault()
+    setBtnMessage('Sending...')
 
-    emailjs
-      .sendForm(
-        serviceId,
-        templateId,
-        form.current,
-        apiKey
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setBtnMessage('Message Sent!');
-          setTimeout(() => {
-            setBtnMessage('Send');
-          }, 2000);
+    emailjs.sendForm(serviceId, templateId, form.current, apiKey).then(
+      (result) => {
+        console.log(result.text)
+        setBtnMessage('Message Sent!')
+        setTimeout(() => {
+          setBtnMessage('Send')
+        }, 2000)
+      },
+      (error) => {
+        console.log(error.text)
+      }
+    )
+  }
 
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
-
-    return (
-      <div id='contact' className="h-full min-h-[600px] w-screen max-w-[800px] flex flex-col bg-[#f2f2f2] rounded-md  mb-40 p-4 shadow-md shadow-gray scale-[90%] sm:scale-[80%] lg:scale-100 mt-20">
-        <div className="flex justify-between">
-          <h2 className="text-3xl  text-black h-20">Let&apos;s Connect!</h2>
-          <p className='text-xl font-semibold hidden sm:block'>Joshua Nelson</p>
-        </div>
-        <form className="flex flex-col gap-8 rounded-md" ref={form} onSubmit={sendEmail}>
-          <div className='grid gap-2'>
-            <label htmlFor='from_name' className='text-black dark:text-black'>Name</label>
-            <input name='from_name' id='from_name' className='rounded border border-[#ccc] box-border text-black outline-primary py-1 px-3 shadow' type="text" required />
-          </div> 
-          <div className='grid gap-2'>
-            <label htmlFor="email" className='text-black dark:text-black'>Email</label>
-            <input className='rounded border border-[#ccc] box-border text-black outline-primary py-1 px-3 shadow' type="email" name="user_email" id='email' required />
-          </div> 
-          <div className='grid gap-2'>
-            <label htmlFor='message' className='text-black dark:text-black'>Message</label>
-            <textarea name='message' id='message' className='rounded h-60 text-black p-4 border border-[#ccc] shadow outline-primary resize-none' required />
-          </div> 
-          <input className='bg-primary rounded p-2 text-darkGray font-semibold shadow shadow-gray md:bg-opacity-90 md:hover:bg-opacity-100 cursor-pointer' type="submit" value={btnMessage} />
-        </form>
+  return (
+    <div
+      id="contact"
+      className="h-full min-h-[600px] w-screen max-w-[800px] flex flex-col bg-[#f2f2f2] rounded-md  mb-40 p-4 shadow-md shadow-gray scale-[90%] sm:scale-[80%] lg:scale-[80%] mt-20"
+    >
+      <div className="flex justify-between">
+        <h2 className="text-3xl  text-black h-20">Let&apos;s Connect!</h2>
+        <p className="text-xl font-semibold hidden sm:block">Joshua Nelson</p>
       </div>
-    );
+      <form
+        className="flex flex-col gap-8 rounded-md"
+        ref={form}
+        onSubmit={sendEmail}
+      >
+        <div className="grid gap-2">
+          <label htmlFor="from_name" className="text-black dark:text-black">
+            Name
+          </label>
+          <input
+            name="from_name"
+            id="from_name"
+            className="rounded border border-[#ccc] box-border text-black outline-primary py-1 px-3 shadow"
+            type="text"
+            required
+          />
+        </div>
+        <div className="grid gap-2">
+          <label htmlFor="email" className="text-black dark:text-black">
+            Email
+          </label>
+          <input
+            className="rounded border border-[#ccc] box-border text-black outline-primary py-1 px-3 shadow"
+            type="email"
+            name="user_email"
+            id="email"
+            required
+          />
+        </div>
+        <div className="grid gap-2">
+          <label htmlFor="message" className="text-black dark:text-black">
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            className="rounded h-60 text-black p-4 border border-[#ccc] shadow outline-primary resize-none"
+            required
+          />
+        </div>
+        <input
+          className="bg-primary rounded p-2 text-darkGray font-semibold shadow shadow-gray md:bg-opacity-90 md:hover:bg-opacity-100 cursor-pointer"
+          type="submit"
+          value={btnMessage}
+        />
+      </form>
+    </div>
+  )
 }
-
--- src/Components/PortfolioComponent.jsx
+++ src/Components/PortfolioComponent.jsx
@@ -1,5 +1,5 @@
 // PortfolioComponent.jsx
-
+import { Link } from 'react-router-dom'
 import PantriLanding from '../assets/images/pantri-landing.jpeg'
 import PantriProducts from '../assets/images/pantri-products.jpeg'
 
@@ -7,18 +7,26 @@ export default function PortfolioComponent() {
   return (
     <div
       id="portfolio"
-      className="h-auto min-h-[600px] w-full flex flex-col items-center mb-12 md:mb-20 md:mt-24"
+      className="h-auto min-h-[600px] w-full flex flex-col items-center mb-12 md:mb-4 md:mt-24"
     >
       <h1 className="text-4xl lg:text-6xl font-semibold text-darkGray dark:text-primary mb-4 md:mb-10">
         Portfolio
       </h1>
-      <div className="grid place-content-center lg:px-20 lg:grid-cols-2 gap-10">
-        <img src={PantriLanding} alt="Pantri Landing Page" className={image} />
-        <img src={PantriProducts} alt="Pantri Products" className={image} />
+      <div className="grid place-content-center lg:px-20 lg:grid-cols-1 gap-10">
+        <Link to="https://github.com/joshuanelsondev/portfolio-frontend/blob/main/src/assets/images/pantri-landing.jpeg?raw=true">
+          <img
+            src={PantriLanding}
+            alt="Pantri Landing Page"
+            className={image}
+          />
+        </Link>
+        <Link to="https://github.com/joshuanelsondev/portfolio-frontend/blob/main/src/assets/images/pantri-products.jpeg?raw=true">
+          <img src={PantriProducts} alt="Pantri Products" className={image} />
+        </Link>
       </div>
     </div>
   )
 }
 
 // Tailwind Styles
-const image = 'h-[16em] w-[22em] rounded-lg'
+const image = 'h-[18em] w-[24em] rounded-lg cursor-pointer'
